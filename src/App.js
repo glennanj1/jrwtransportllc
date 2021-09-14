b432f33880cb05cc40c026c425d5ef5597dc9784
john glennan <glennan20@gmail.com>
2021-09-14 14:24:21 -0400

    comment

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+// import IconButton from '@material-ui/core/IconButton';
+// import MenuIcon from '@material-ui/icons/Menu';
 import Cards from './Cards'
 import bgpic from './images/jrwpic.jpg'
 import dry from './images/drybulk.jpg'
