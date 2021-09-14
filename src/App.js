159de949b6fc7716e88329937e4c31423c18c4ff
john glennan <glennan20@gmail.com>
2021-09-14 14:23:53 -0400

    changes and styling

--- src/App.js
+++ src/App.js
@@ -1,28 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Cards from './Cards'
+import bgpic from './images/jrwpic.jpg'
+import dry from './images/drybulk.jpg'
+import orange from './images/orangetruck.png'
+import white from './images/whitetruck.png'
+import RoomIcon from '@material-ui/icons/Room';
 
-function App() {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    width: '100vw',
+    height: '100vh',
+    backgroundImage: `url(${bgpic})`,
+    backgroundPosition: 'center',
+    backgroundSize: 'cover'
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  header: {
+    display: 'flex',
+    justifyContent: 'center',
+    textAlign: 'center',
+    color: 'white',
+    textShadow: '-3px -3px 0 #000, 3px -3px 0 #000, -3px 3px 0 #000, 3px 3px 0 #000',
+    fontFamily: 'Roboto',
+    fontSize: '8vw',
+    padding: '25px',
+  },
+  div: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  appbar: {
+    background: '#014f99b8',
+  }
+}));
+
+export default function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Commercial transportation company specializing in Dry Bulk, 
-          Refrigerated & Heavy duty Hauling since 2012. 
-          Let us assist you with your transportation needs in a safer 
-          punctual cost effective manner.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn More
-        </a>
-      </header>
+    <div className={classes.root}>
+      <AppBar className={classes.appbar} position="static">
+        <Toolbar>
+          {/* <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton> */}
+          <Typography variant="h6" className={classes.title}>
+            JRW Transport LLC
+          </Typography>
+          <Button color="inherit" title="Google Maps"><RoomIcon /></Button>
+        </Toolbar>
+      </AppBar>
+      <h1 className={classes.header}>
+          Commercial transportation company 
+      </h1>
+      <div className={classes.div}>
+        <Cards title="Dry Bulk" text="Our services are top notch" image={dry} />
+        <Cards title="Refridgerated" text="Keeping Your Product Cool" image={white} />
+        <Cards title="Hauling" text="Haul weights up to XXX" image={orange} />
+      </div>
+
     </div>
   );
 }
-
-export default App;
